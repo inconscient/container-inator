1bc680e8d67ec500f8d5c01301ce6151ed454189
Inconscient <andres@ingeniousconnections.com>
2024-10-03 12:20:09 -0500

    First version deployed to docker registry

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace container_lab_web.Pages
 {
